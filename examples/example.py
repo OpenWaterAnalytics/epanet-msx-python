9f3df9a070cd57de37dca6519f4f243d23a83cab
Kyle Arrowood <kyle.arrowood@xylem.com>
2021-08-03 12:05:11 -0400

    Added another example to the examples folder, both examples work as expected.

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,4 @@
 from epanetmsx import toolkit as msx
-import ctypes
 
 MINUTE = 60
 HOUR = 60 * 60
@@ -10,128 +9,109 @@ def make_array(values):
         arr[i] = values[i]
     return arr
 
-def batchExample(fname):
+def example(fname):
     err = 0
     msx.open()
 
-
-    # Builing the network from batch-nh2cl.inp
-    msx.setFlowFlag(msx.GPM)
-    msx.setTimeParameter(msx.DURATION, 168*HOUR)
+    # Builing the network from example.inp
+    msx.setFlowFlag(msx.CMH)
+    msx.setTimeParameter(msx.DURATION, 80*HOUR)
     msx.setTimeParameter(msx.HYDSTEP, 1*HOUR)
-    msx.setTimeParameter(msx.QUALSTEP, 5*MINUTE)
-    msx.setTimeParameter(msx.REPORTSTEP, 1*HOUR)
+    msx.setTimeParameter(msx.QUALSTEP, 8*HOUR)
+    msx.setTimeParameter(msx.REPORTSTEP, 8*HOUR)
     msx.setTimeParameter(msx.REPORTSTART, 0)
-    msx.setTimeParameter(msx.PATTERNSTEP, 1*HOUR)
-    msx.setTimeParameter(msx.PATTERNSTART, 0)
 
     # Add nodes
-    msx.addNode("2")
-    msx.addTank("1", 19634.953125,0,39269.906250)
-
+    msx.addNode("a")
+    msx.addNode("b")
+    msx.addNode("c")
+    msx.addNode("e")
+    msx.addReservoir("source", 0,0,0)
     # Add links
-    msx.addLink("1", "1", "2", 1000, 12, 100)
+    msx.addLink("1", "source", "a", 1000, 200, 100)
+    msx.addLink("2", "a", "b", 800, 150, 100)
+    msx.addLink("3", "a", "c", 1200, 200, 100)
+    msx.addLink("4", "b", "c", 1000, 150, 100)
+    msx.addLink("5", "c", "e", 2000, 150, 100)
 
     # Add Options
-    msx.addOption(msx.AREA_UNITS_OPTION, "FT2")
+    msx.addOption(msx.AREA_UNITS_OPTION, "M2")
     msx.addOption(msx.RATE_UNITS_OPTION, "HR")
-    msx.addOption(msx.SOLVER_OPTION, "ROS2")
-    msx.addOption(msx.COUPLING_OPTION, "FULL")
-    msx.addOption(msx.TIMESTEP_OPTION, "300")
-    msx.addOption(msx.RTOL_OPTION, "0.0001")
-    msx.addOption(msx.ATOL_OPTION, "1.0e-8")
+    msx.addOption(msx.SOLVER_OPTION, "RK5")
+    msx.addOption(msx.TIMESTEP_OPTION, "28800")
+    msx.addOption(msx.RTOL_OPTION, "0.001")
+    msx.addOption(msx.ATOL_OPTION, "0.0001")
 
     # Add Species
-    msx.setSize(msx.SPECIES, 14)
-    msx.addSpecies("HOCL", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("NH3", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("NH2CL", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("NHCL2", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("I", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("OCL", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("NH4", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("ALK", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("H", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("OH", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("CO3", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("HCO3", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("H2CO3", msx.BULK, msx.MOLE, 0.0, 0.0)
-    msx.addSpecies("chloramine", msx.BULK, msx.MMOLE, 0.0, 0.0)
-
+    msx.addSpecies("AS3", msx.BULK, msx.UG, 0.0, 0.0)
+    msx.addSpecies("AS5", msx.BULK, msx.UG, 0.0, 0.0)
+    msx.addSpecies("AStot", msx.BULK, msx.UG, 0.0, 0.0)
+    msx.addSpecies("AS5s", msx.WALL, msx.UG, 0.0, 0.0)
+    msx.addSpecies("NH2CL", msx.BULK, msx.MG, 0.0, 0.0)
+    
     #Add Coefficents
-    msx.addCoefficeint(msx.PARAMETER, "k1", 1.5e10)
-    msx.addCoefficeint(msx.PARAMETER, "k2", 7.6e-2)
-    msx.addCoefficeint(msx.PARAMETER, "k3", 1.0e6)
-    msx.addCoefficeint(msx.PARAMETER, "k4", 2.3e-3)
-    msx.addCoefficeint(msx.PARAMETER, "k6", 2.2e8)
-    msx.addCoefficeint(msx.PARAMETER, "k7", 4.0e5)
-    msx.addCoefficeint(msx.PARAMETER, "k8", 1.0e8)
-    msx.addCoefficeint(msx.PARAMETER, "k9", 3.0e7)
-    msx.addCoefficeint(msx.PARAMETER, "k10", 55.0)
+    msx.addCoefficeint(msx.CONSTANT, "Ka", 10.0)
+    msx.addCoefficeint(msx.CONSTANT, "Kb", 0.1)
+    msx.addCoefficeint(msx.CONSTANT, "K1", 5.0)
+    msx.addCoefficeint(msx.CONSTANT, "K2", 1.0)
+    msx.addCoefficeint(msx.CONSTANT, "Smax", 50)
 
     #Add terms
-    msx.addTerm("k5", "(2.5e7*H) + (4.0e4*H2CO3) + (800*HCO3)")
-    msx.addTerm("a1", "k1*HOCL*NH3")
-    msx.addTerm("a2", "k2*NH2CL")
-    msx.addTerm("a3", "k3*HOCL*NH2CL")
-    msx.addTerm("a4", "k4*NHCL2")
-    msx.addTerm("a5", "k5*NH2CL*NH2CL")
-    msx.addTerm("a6", "k6*NHCL2*NH3*H")
-    msx.addTerm("a7", "k7*NHCL2*OH")
-    msx.addTerm("a8", "k8*I*NHCL2")
-    msx.addTerm("a9", "k9*I*NH2CL")
-    msx.addTerm("a10", "k10*NH2CL*NHCL2")
+    msx.addTerm("Ks", "K1/K2")
 
     #Add Expressions
-    msx.addExpression(msx.LINK, msx.RATE, "HOCL", "-a1 + a2 - a3 + a4 + a8")
-    msx.addExpression(msx.LINK, msx.RATE, "NH3", "-a1 + a2 + a5 - a6")
-    msx.addExpression(msx.LINK, msx.RATE, "NH2CL", "a1 - a2 - a3 + a4 - a5 + a6 - a9 - a10")
-    msx.addExpression(msx.LINK, msx.RATE, "NHCL2", "a3 - a4 + a5 - a6 - a7 - a8 - a10")
-    msx.addExpression(msx.LINK, msx.RATE, "I", "a7 - a8 - a9")
-    msx.addExpression(msx.LINK, msx.RATE, "H", "0")
-    msx.addExpression(msx.LINK, msx.RATE, "ALK", "0")
+    msx.addExpression(msx.LINK, msx.RATE, "AS3", "-Ka*AS3*NH2CL")
+    msx.addExpression(msx.LINK, msx.RATE, "AS5", "Ka*AS3*NH2CL-Av*(K1*(Smax-AS5s)*AS5-K2*AS5s)")
+    msx.addExpression(msx.LINK, msx.RATE, "NH2CL", "-Kb*NH2CL")
+    msx.addExpression(msx.LINK, msx.EQUIL, "AS5s", "Ks*Smax*AS5/(1+Ks*AS5)-AS5s")
+    msx.addExpression(msx.LINK, msx.FORMULA, "AStot", "AS3 + AS5")
 
-    msx.addExpression(msx.LINK, msx.EQUIL, "OCL", "H*OCL - 3.16E-8*HOCL")
-    msx.addExpression(msx.LINK, msx.EQUIL, "NH4", "H*NH3 - 5.01E-10*NH4")
-    msx.addExpression(msx.LINK, msx.EQUIL, "CO3", "H*CO3 - 5.01E-11*HCO3")
-    msx.addExpression(msx.LINK, msx.EQUIL, "H2CO3", "H*HCO3 - 5.01E-7*H2CO3")
-    msx.addExpression(msx.LINK, msx.EQUIL, "HCO3", "ALK - HCO3 - 2*CO3 - OH + H")
-    msx.addExpression(msx.LINK, msx.EQUIL, "OH", "H*OH - 1.0E-14")
+    msx.addExpression(msx.TANK, msx.RATE, "AS3", "-Ka*AS3*NH2CL")
+    msx.addExpression(msx.TANK, msx.RATE, "AS5", "Ka*AS3*NH2CL")
+    msx.addExpression(msx.TANK, msx.RATE, "NH2CL", "-Kb*NH2CL")
+    msx.addExpression(msx.TANK, msx.FORMULA, "AStot", "AS3+AS5")
 
-    msx.addExpression(msx.LINK, msx.FORMULA, "chloramine", "1000*NH2CL")
-
-    
     #Add Quality
-    msx.addQuality("GLOBAL", "NH2CL", 0.05E-3, "")
-    msx.addQuality("GLOBAL", "ALK", 0.004, "")
-    msx.addQuality("GLOBAL", "H", 2.82e-8, "")
+    msx.addQuality("NODE", "AS3", 10.0, "source")
+    msx.addQuality("NODE", "NH2CL", 2.5, "source")
+
+    #Setup Report
+    msx.setReport("NODE", "c", 2)
+    msx.setReport("NODE", "e", 2)
+    msx.setReport("LINK", "5", 2)
+    msx.setReport("SPECIE", "AStot", 2)
+    msx.setReport("SPECIE", "AS3", 2)
+    msx.setReport("SPECIE", "AS5", 2)
+    msx.setReport("SPECIE", "AS5s", 2)
+    msx.setReport("SPECIE", "NH2CL", 2)
 
     # Finish Setup
     msx.init()
 
     # Run
-    demands = make_array([0.0, 0.0])
-    heads = make_array([10.0, 10.0])
-    flows = make_array([0.0])
-
+    demands = make_array([0.040220, 0.033353, 0.053953, 0.022562, -0.150088])
+    heads = make_array([327.371979, 327.172974, 327.164185, 326.991211, 328.083984])
+    flows = make_array([0.150088, 0.039916, 0.069952, 0.006563, 0.022562])
     msx.setHydraulics(demands, heads, flows)
-
     t = 0
     tleft = 1
     oldHour = -1
     newHour = 0
 
     # Example of using the printQuality function in the loop rather than
-    # saving results to binary out file and then calling the msx.report function
+    # saving results to binary out file and then calling the MSXreport function
     while (tleft >= 0 and err == 0):
         if ( oldHour != newHour ):
-            print(f"\r  o Computing water quality at hour {newHour}", flush = True)
-            msx.printQuality(msx.NODE, "1", "chloramine", fname)
+            print(f"\r  o Computing water quality at hour {newHour}", flush=True)
+            msx.printQuality(msx.LINK, "4", "AS5s", fname)
+            msx.printQuality(msx.LINK, "5", "AS5s", fname)
             oldHour = newHour
         t, tleft = msx.step(t, tleft)
-        newHour = t // 3600
-    
+        newHour = t / 3600
     print("\n")
+
+
+
 
 
     # Close
@@ -142,4 +122,4 @@ def batchExample(fname):
 # Main
 if __name__ == "__main__":
     err = 0
-    err = batchExample("")
+    err = example("")
